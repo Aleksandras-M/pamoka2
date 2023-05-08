d5250060860d42fb4f457f7799704fd293acf4f0
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-08 18:13:07 +0300

    p13 u1

---
+++ pamoka13/uzduotis1.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+    let x = prompt("Įveskite skaičių");
+
+    if (x >= 0) {
+        alert("Skaičius yra teigiamas");
+    } else {
+        alert("Skaičius yra neigiamas");
+    }
+
+}
+
