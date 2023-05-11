8ded28d0e111bf7d4b923d91c9c8bba305d2f282
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-11 20:08:15 +0300

    p15 u1 ir u2

---
+++ pamoka15/uzduotis1.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    let z = prompt("Įveskite žodį")
+    alert(`Žodžio ${z} ilgis yra ${z.length}`);
+    
+}
+
---
+++ pamoka15/uzduotis2.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    let z = prompt("Įveskite žodį")
+    console.log(`Žodžio ${z} paskutinė raidė yra ${z.at(-1)}`);
+    
+}
+
