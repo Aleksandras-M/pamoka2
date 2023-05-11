9e87997216ef3e37502feb6b4f6c132b01ca18e7
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-11 20:26:30 +0300

    p15 u5

---
+++ pamoka15/uzduotis5.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+    let z = prompt("Įveskite žodį");
+    let z1 = prompt("Įveskite ieškomą žodio galūnę")
+    console.log(`Žodis ${z} baigiasi ${z1} : ${z.toLocaleLowerCase().endsWith(z1.toLocaleLowerCase())}`);
+    
+}
+
