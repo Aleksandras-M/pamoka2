65364c0f18b29c0e6a26f270a3f72858a9ca1575
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-15 21:39:54 +0300

    p16 u1

---
+++ uzduotis1.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+    let pradzia = +prompt(`Įveskite sekos pradžią`);
+    let pabaiga = +prompt(`Įveskite sekos pabaigą`);
+    let intervalas = +prompt(`Įveskite intervalą`)
+    rastiSkaiciu(pradzia, pabaiga, intervalas)
+}
+
+function rastiSkaiciu(a, b, c) {
+    for(i = a; i < b; i += c){
+        console.log(i);
+    }
+}
