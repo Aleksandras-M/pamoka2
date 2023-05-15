3e3459f1e8fd6788b6bc11ed7404895b3559c5c2
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-15 21:44:36 +0300

    p16 u2

---
+++ pamoka16/uzduotis2.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+    let pradzia = +prompt(`Įveskite sekos pradžią`);
+    let pabaiga = +prompt(`Įveskite sekos pabaigą`);
+    let intervalas = +prompt(`Įveskite intervalą`)
+    rastiSkaiciuSuma(pradzia, pabaiga, intervalas)
+}
+
+function rastiSkaiciuSuma(a, b, c) {
+    let suma = 0
+    for(i = a; i < b; i += c){
+        suma += i;
+    }
+    console.log(suma);
+}
