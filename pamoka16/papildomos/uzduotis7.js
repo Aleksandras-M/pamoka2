f6405154e7a3491d08e4c8cddd6ef1360f0e6f9f
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-16 12:55:30 +0300

    p16 papildoma u7

---
+++ pamoka16/papildomos/uzduotis7.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+
+    let pradzia = +prompt(`Įveskite intervalo pradžią`);
+    let pabaiga = +prompt(`Įveskite intervalo pabaigą`);
+    
+    teisingasSumosAtsakymas(pradzia, pabaiga)
+    // console.log(atsitiktinisSveikasSkaicius(pradzia, pabaiga))
+    
+}
+
+function atsitiktinisSveikasSkaicius(pradzia, pabaiga) {
+    return Math.round(Math.random() * (pabaiga - pradzia) + pradzia)
+
+}
+
+function teisingasSumosAtsakymas(x, y){
+    let a = atsitiktinisSveikasSkaicius(x, y);
+    let b = atsitiktinisSveikasSkaicius(x, y);
+    let c = a + b
+
+    let ivestis = +prompt(`Įveskite skaičių ${a} ir ${b} sumą`)
+    
+    while(c != ivestis){
+        ivestis = +prompt(`Įveskite skaičių ${a} ir ${b} sumą`)
+    }
+}
