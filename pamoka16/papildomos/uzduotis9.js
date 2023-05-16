3da67682dc6803d2afd094e6862fc9c3421b99fd
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-16 20:27:49 +0300

    p16 papildoma u9

---
+++ pamoka16/papildomos/uzduotis9.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+    let z = prompt(`Įveskite tekstą`)
+    // console.log(atvirkstinisTekstas(z));
+    console.log(`${z} yra palindromas: ${z == atvirkstinisTekstas(z)}`)
+}
+
+function atvirkstinisTekstas(z) {
+    let atvirksciasZ = "";
+    for(i = z.length - 1; i >= 0; i--){
+        atvirksciasZ += z.at(i);
+    }
+    return atvirksciasZ;
+}
