eee88c4abbb774222e94b1787925b6780026b4d5
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-26 10:29:36 +0300

    p22 u4

---
+++ JAVA/pamoka22/src/uzduotis4.java
@@ -0,0 +1,15 @@
+public class uzduotis4 {
+    public static void main(String[] args) {
+        int a1 = 25;
+        int b1 = 37;
+        System.out.println(padvigubintiSuma(a1, b1));
+        int a2 = -200;
+        int b2 = 100;
+        System.out.println(padvigubintiSuma(a2, b2));
+    }
+    public static int padvigubintiSuma(int a, int b){
+        return (a + b) * 2;
+    }
+}
+
+
