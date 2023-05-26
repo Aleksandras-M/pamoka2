869322fd41579e72c4a53d7278f7d2a70440585a
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-26 09:14:29 +0300

    p22 u1 ir u2

---
+++ JAVA/pamoka22/src/uzduotis1.java
@@ -0,0 +1,11 @@
+public class uzduotis1 {
+    public static void main(String[] args) {
+
+
+        System.out.println(15);
+        System.out.println("Labas rytas");
+        System.out.println("Namas");
+        System.out.println(5 + 7);
+        System.out.println("namas" + " " + "namas");
+    }
+}
---
+++ JAVA/pamoka22/src/uzduotis2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class uzduotis2 {
+    public static <string> void main(String[] args) {
+        short s1 = 25;
+        System.out.println(s1);
+        boolean bool1 = true;
+        System.out.println(bool1);
+        String tekstas1 = "Labas rytas";
+        System.out.println(tekstas1);
+        double d1 = 2548951;
+        System.out.println(d1);
+        short s2 = 5688;
+        int i1 = (int) s2;
+        System.out.println(i1);
+        int i2 = 562;
+        byte b1 = (byte) i2;
+        System.out.println(b1);
+
+    }
+}
