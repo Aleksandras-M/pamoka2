69a536cdb66c34fdaf8aae17426a7d4a75104460
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-29 09:35:45 +0300

    p23 u3

--- JAVA/pamoka23-java-salyginiai-sakiniai-palyginimai/src/uzduotis3.java
+++ JAVA/pamoka23-java-salyginiai-sakiniai-palyginimai/src/uzduotis3.java
@@ -1,5 +1,13 @@
 public class uzduotis3 {
     public static void main(String[] args) {
-        
+        int x1 = 20;
+        int y1 = 5;
+        int x2 = 14;
+        int y2 = 6;
+        System.out.println(arKartotinis(x1, y1));
+        System.out.println(arKartotinis(x2, y2));
+    }
+    public static boolean arKartotinis(int x, int y){
+        return x % y == 0;
     }
 }
