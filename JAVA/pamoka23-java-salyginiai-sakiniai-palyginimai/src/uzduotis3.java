d4b283867abd466d9fbfd7df7fd231ffba3505c9
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-29 18:13:56 +0300

    p23 u3 keitimas is return i if else

--- JAVA/pamoka23-java-salyginiai-sakiniai-palyginimai/src/uzduotis3.java
+++ JAVA/pamoka23-java-salyginiai-sakiniai-palyginimai/src/uzduotis3.java
@@ -3,11 +3,20 @@ public class uzduotis3 {
         int x1 = 20;
         int y1 = 5;
         int x2 = 14;
-        int y2 = 6;
+        int y2 = 3;
         System.out.println(arKartotinis(x1, y1));
         System.out.println(arKartotinis(x2, y2));
     }
-    public static boolean arKartotinis(int x, int y){
-        return x % y == 0;
+
+    public static String arKartotinis(int x, int y) {
+        if (y == 0) {
+            return "Dalyba iš 0 negalima";
+        } else {
+            if (x % y == 0) {
+                return "true";
+            } else {
+                return "false";
+            }
+        }
     }
 }
