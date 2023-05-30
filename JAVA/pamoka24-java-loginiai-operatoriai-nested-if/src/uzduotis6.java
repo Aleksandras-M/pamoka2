810801807fe9c91e0cfa6b7e4be5f9600d1a9441
Aleksandras-M <alek.macijauskas@gmail.com>
2023-05-30 15:29:47 +0300

    p24 u6

---
+++ JAVA/pamoka24-java-loginiai-operatoriai-nested-if/src/uzduotis6.java
@@ -0,0 +1,17 @@
+public class uzduotis6 {
+    public static void main(String[] args) {
+        arKeliamieji(1602);
+        arKeliamieji(2000);
+        arKeliamieji(1500);
+        arKeliamieji(1600);
+    }
+    
+    public static void arKeliamieji(int x){
+        if(x >= 1582){
+            System.out.println("arKeliamieji (" + x + ") = " + (x % 4 == 0));
+        }
+    }
+    public static void atskirimas() {
+        System.out.println("----------------");
+    }
+}
