1a45daffd708398e3a97e58cd948e5e5a5136ac8
Aleksandras-M <alek.macijauskas@gmail.com>
2023-06-01 18:52:02 +0300

    p26 u1

--- JAVA/Pamoka26-tekstiniai-kintamieji/src/uzduotis1.java
+++ JAVA/Pamoka26-tekstiniai-kintamieji/src/uzduotis1.java
@@ -1,2 +1,14 @@
 public class uzduotis1 {
+    public static void main(String[] args) {
+        String zodis1 = "Antanas";
+        int x1 = 5;
+        String zodis2 = "ropė";
+        int x2 = 7;
+        System.out.println("arZodisIlgesnisNeiSkaicius(zodis1, x1) = " + arZodisIlgesnisNeiSkaicius(zodis1, x1));
+        System.out.println("arZodisIlgesnisNeiSkaicius(zodis2, x2) = " + arZodisIlgesnisNeiSkaicius(zodis2, x2));
+    }
+
+    public static boolean arZodisIlgesnisNeiSkaicius(String zodis, int x){
+        return zodis.length() > x;
+    }
 }
