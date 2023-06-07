b09a790ad2c140d27d628151faec3a58e5791582
Aleksandras-M <alek.macijauskas@gmail.com>
2023-06-07 12:03:21 +0300

    p28 u4-u5

--- JAVA/pamoka28-masyvai/src/uzduotis1.java
+++ JAVA/pamoka28-masyvai/src/uzduotis1.java
@@ -10,10 +10,19 @@ public class uzduotis1 {
         int[] y1 = {3, 5, 8};
         int[] y2 = {2, 7, 12, 9};
         System.out.println("Arrays.toString(masyvuSudejimas(y1, y2)) = " + Arrays.toString(masyvuSudejimas(y1, y2)));
+        System.out.println("masyvuSudejimasBeSukurtuJavaFunkciju(y1, y2) = " +
+                Arrays.toString(masyvuSudejimasBeSukurtuJavaFunkciju(y1, y2)));
 
         double[] y3 = {32.4, 14.9, 7.6, 43.2, 25.3, 19.8, 38.1, 2.7, 46.5, 9.2};
-        System.out.println("masyvoElementuSumaSuFor(y3) = " + masyvoElementuSumaSuFor(y3));
-        System.out.println("masyvoElementuSumaSuWhile(y3) = " + masyvoElementuSumaSuWhile(y3));
+        System.out.println("masyvoElementuSumaSuFor(y3) = " + suapvalinti(masyvoElementuSumaSuFor(y3)));
+        System.out.println("masyvoElementuSumaSuWhile(y3) = " + suapvalinti(masyvoElementuSumaSuWhile(y3)));
+
+        int[] masyvasSuNeigimais = {-12, 38, 5, -45, 28, 9, -34, 16, 47, -22, 41, -2, -17, 30, -41, -6, 19, 33, 0, -29};
+        System.out.println("neigiamuElementuKeitimasITeigiamusMasyve(masyvasSuNeigimais) = " +
+                Arrays.toString(neigiamuElementuKeitimasITeigiamusMasyve(masyvasSuNeigimais)));
+
+        String[] zodziai = {"kelmas", "botAGAS", "laumė", "mašina", "burlaivis"};
+        System.out.println("kiekMasyveVyriskosGiminesZodziu(zodziai) = " + kiekMasyveVyriskosGiminesZodziu(zodziai));
     }
 
     public static boolean arPirminisSkaicius(int x) {
@@ -45,6 +54,23 @@ public class uzduotis1 {
         return rezultatas;
     }
 
+    public static int[] masyvuSudejimasBeSukurtuJavaFunkciju(int[] a, int[] b){
+        int[] rezultatas = new int[a.length + b.length];
+        int i = 0;
+
+        for(int ele: a){
+            rezultatas[i] = ele;
+            i++;
+        }
+
+        for(int ele: b){
+            rezultatas[i] = ele;
+            i++;
+        }
+
+        return rezultatas;
+    }
+
     public static double masyvoElementuSumaSuFor(double[] a){
         double suma = 0;
         for(double i : a) {
@@ -61,4 +87,37 @@ public class uzduotis1 {
         }
         return suma;
     }
+
+    public static boolean arSkaiciusNeigiamas(int x){
+        return x < 0;
+    }
+
+    public static int[] neigiamuElementuKeitimasITeigiamusMasyve(int[] a){
+        for (int i = 0; i < a.length; i++) {
+            if (arSkaiciusNeigiamas(a[i])) {
+                a[i] = Math.abs(a[i]);
+            }
+        }
+        return a;
+    }
+
+    public static boolean arZodisVyriskosGimines(String z){
+        return (z.toLowerCase().endsWith("as") || z.toLowerCase().endsWith("us") || z.toLowerCase().endsWith("is"));
+    }
+
+    public static int kiekMasyveVyriskosGiminesZodziu(String[] a){
+        int kiekis = 0;
+        for (String s : a) {
+            if (arZodisVyriskosGimines(s)) {
+                kiekis++;
+            }
+        }
+        return kiekis;
+    }
+
+    public static double suapvalinti(double x){
+        return Math.round(x * 100d) / 100d;
+    }
+
+
 }
